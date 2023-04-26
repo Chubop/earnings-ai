5c2a942e0099ee8444663f737cbd99857b182cb4
Chubop <samuelokirby@gmail.com>
2023-04-26 18:27:10 -0400

    added lpsn transcript and added token price tracking

--- main.py
+++ main.py
@@ -1,21 +1,34 @@
 from langchain.document_loaders import TextLoader
 from langchain.indexes import VectorstoreIndexCreator
+from langchain.llms import OpenAI
+from langchain.callbacks import get_openai_callback
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+
 import os
 
 
-
-def init(text: str):
+def init(text: str, llm):
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=40,
+        chunk_overlap=20,
+        length_function=len,
+    )
+    texts = text_splitter.create_documents([text])
     loader = TextLoader(text)
     index = VectorstoreIndexCreator().from_loaders([loader])
     while True:
-        query = input("Please enter your question regarding the earnings call.\n: ")
-        answer = index.query_with_sources(query)
-        print(answer['answer'])
+        with get_openai_callback() as cb:
+            query = input("Please enter your question regarding the earnings call.\n: ")
+            answer = index.query_with_sources(query, llm=llm)
+            print(answer['answer'])
+            print(cb)
 
 
 if __name__ == '__main__':
-    # init('transcripts/lpsn_transcript')
+    llm = OpenAI(model_name="text-davinci-002", n=2, best_of=2, temperature=0)
+    init('transcripts/lpsn_transcript', llm=llm)
     with open('transcripts/lpsn_transcript', 'r') as outfile:
         for x in outfile.readlines():
             if x != '\n':
                 print(x, end='')
+
