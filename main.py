68f414d32d5a780673ac98800997d1275a37d5da
Chubop <samuelokirby@gmail.com>
2023-05-03 13:51:08 -0400

    basic question answering support

--- main.py
+++ main.py
@@ -1,34 +1,46 @@
-from langchain.document_loaders import TextLoader
+import openai, vector_db
 from langchain.indexes import VectorstoreIndexCreator
 from langchain.llms import OpenAI
 from langchain.callbacks import get_openai_callback
 from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.document_loaders import DirectoryLoader
+from langchain.chains.question_answering import load_qa_chain
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import Chroma
+from langchain.chains import RetrievalQA
+from langchain.vectorstores.base import VectorStoreRetriever
 
-import os
+
+def load_docs(directory: str = "transcripts/LPSN/2022"):
+    loader = DirectoryLoader(directory)
+    documents = loader.load()
+    return documents
 
 
-def init(text: str, llm):
-    text_splitter = RecursiveCharacterTextSplitter(
-        chunk_size=40,
-        chunk_overlap=20,
-        length_function=len,
-    )
-    texts = text_splitter.create_documents([text])
-    loader = TextLoader(text)
-    index = VectorstoreIndexCreator().from_loaders([loader])
-    while True:
-        with get_openai_callback() as cb:
-            query = input("Please enter your question regarding the earnings call.\n: ")
-            answer = index.query_with_sources(query, llm=llm)
-            print(answer['answer'])
-            print(cb)
+def split_docs(documents, chunk_size=1000, chunk_overlap=20):
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=chunk_size,
+                                                   chunk_overlap=chunk_overlap)
+    docs = text_splitter.split_documents(documents=documents)
+    return docs
+
+
+def load_embeddings(model_name: str = "ada"):
+    return OpenAIEmbeddings()
+
+
+def init():
+    documents = load_docs()
+    docs = split_docs(documents)
+    embedding = load_embeddings()
+    Index = vector_db.Index(documents=docs, embedding=embedding)
+    return Index
 
 
 if __name__ == '__main__':
-    llm = OpenAI(model_name="text-davinci-002", n=2, best_of=2, temperature=0)
-    init('transcripts/lpsn_transcript', llm=llm)
-    with open('transcripts/lpsn_transcript', 'r') as outfile:
-        for x in outfile.readlines():
-            if x != '\n':
-                print(x, end='')
+    # llm = OpenAI(model_name="text-davinci-002", n=2, best_of=2, temperature=0)
+    Index = init()
+    query = Index.index.query()
+    print(query)
+    # print(Index.ask_question("What is LivePerson's forward earnings indicate about its company?", verbose=True))
 
