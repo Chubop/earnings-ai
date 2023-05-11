f9bf23dfbebd28cd8a4044b24a0417e3e29aef1f
Chubop <samuelokirby@gmail.com>
2023-05-11 19:51:31 -0400

    started chat support

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
-import openai, vector_db
-from langchain.indexes import VectorstoreIndexCreator
-from langchain.llms import OpenAI
-from langchain.callbacks import get_openai_callback
+import vector_db
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.document_loaders import DirectoryLoader
-from langchain.chains.question_answering import load_qa_chain
 from langchain.embeddings.openai import OpenAIEmbeddings
+from pprint import pprint
+from langchain.memory import ConversationBufferMemory
 
 
 def load_docs(directory):
@@ -14,7 +12,7 @@ def load_docs(directory):
     return documents
 
 
-def split_docs(documents, chunk_size=1000, chunk_overlap=20):
+def split_docs(documents, chunk_size=1500, chunk_overlap=100):
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=chunk_size,
                                                    chunk_overlap=chunk_overlap)
     docs = text_splitter.split_documents(documents=documents)
@@ -25,7 +23,7 @@ def load_embeddings(model_name: str = "ada"):
     return OpenAIEmbeddings()
 
 
-def init(directory: str = "transcripts/LPSN/2022"):
+def init(directory: str = "transcripts/LPSN/2022", chat_history: list = []):
     documents = load_docs(directory=directory)
     docs = split_docs(documents)
     embedding = load_embeddings()
@@ -38,5 +36,4 @@ if __name__ == '__main__':
     Index = init(directory="transcripts/AAPL/2023")
     # query = Index.index.query()
     # print(query)
-    print(Index.ask_question("Who is John Collins?", verbose=True))
-
+    pprint(Index.ask_question("Could you generate a summary of the call?", verbose=True))
--- vector_db.py
+++ vector_db.py
@@ -1,11 +1,33 @@
 import pinecone, os
+from langchain import PromptTemplate
 from langchain.vectorstores import Pinecone
 from langchain.llms import OpenAI
 from langchain.chains.question_answering import load_qa_chain
+from langchain.chains import ConversationalRetrievalChain
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import LLMChain
+from langchain.chains.conversational_retrieval.prompts import CONDENSE_QUESTION_PROMPT, QA_PROMPT
+
 from langchain.callbacks import get_openai_callback
 
+template = """
+You are a tool designed to analyze financial documents (earnings call transcripts, 10-Qs, etc)
+and to answer questions regarding them. Construct your answers in the tone of a financial analyst.
+Answer the question based on the context below. If the question cannot be answered using the information
+provided, respond with "I don't know."
 
-def get_vector_store(documents, embedding, from_existing_index = True):
+Context: 
+
+Question: {query}
+Answer: """
+
+prompt_template = PromptTemplate(
+    input_variables=["query"],
+    template=template
+)
+
+
+def get_vector_store(documents, embedding, from_existing_index=True):
     pinecone.init(
         api_key=os.environ['PINECONE_API_KEY'],
         environment="us-west1-gcp-free"
@@ -18,10 +40,32 @@ def get_vector_store(documents, embedding, from_existing_index = True):
 
 class Index:
 
-    def __init__(self, documents, embedding, model_name: str = "text-davinci-003", namespace: str = "earnings-ai"):
+    def __init__(self, documents, embedding, chat_history = [], model_name: str = "text-davinci-003", namespace: str = "earnings-ai"):
+        def get_chat_history(inputs) -> str:
+            res = []
+            for ai, human in inputs:
+                res.append(f"Human:{human}\nAI:{ai}")
+            return "\n".join(res)
+
         self.index = pinecone.Index(namespace)
         self.vector_store = get_vector_store(documents=documents, embedding=embedding)
-        self.chain = load_qa_chain(llm=OpenAI(model_name=model_name), chain_type="stuff")
+
+        llm = OpenAI(model_name=model_name, temperature=0)
+        self.chat_history = chat_history
+        self.chain = load_qa_chain(
+            llm=llm,
+            chain_type="map_reduce",
+        )
+        self.memory = ConversationBufferMemory(
+            memory_key="chat_history",
+            return_messages=True
+        )
+        self.qa = ConversationalRetrievalChain.from_llm(
+            llm=llm,
+            retriever=self.vector_store.as_retriever(),
+            memory=self.memory,
+            get_chat_history=get_chat_history
+        )
 
         self.embedding = embedding
         self.documents = documents
@@ -37,8 +81,7 @@ class Index:
 
     def ask_question(self, query: str = None, verbose=False):
         with get_openai_callback() as cb:
-            similar_docs = self.get_similar_docs(query)
-            answer = self.chain.run(input_documents=similar_docs, question=query)
+            answer = self.qa({"question": query, "chat_history": self.chat_history})
             return [answer, cb if verbose else None]
 
     def query(self):
@@ -52,9 +95,7 @@ class Index:
 
 
 if __name__ == '__main__':
-
     pinecone.init(api_key=os.environ['PINECONE_API_KEY'])
-    pinecone.delete_index("example-index")
 
     index = pinecone.Index('earnings-ai')
     delete_response = index.delete(delete_all=True, namespace='earnings-ai')
